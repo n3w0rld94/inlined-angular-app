0e2b0b2bafe071458553702bbb809d183c837c64
Ilyasse Fakhreddine <elias.faraone@outlook.com>
2020-04-07 07:01:51 +0200

    Made it work and tested deployment

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -4,6 +4,8 @@ var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'bundled.html',
       inlineSource: '.(js|css)$'
     }),
     new HtmlWebpackInlineSourcePlugin()
