f45025f4dbea0786a7d1d1c36fa077491fafe123
Ilyasse Fakhreddine <elias.faraone@outlook.com>
2020-04-07 03:28:41 +0200

    Added inline plugin

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,18 +1,20 @@
 var merge = require('webpack-merge');
-var webpack = require('webpack');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 exports.default = {
-    config: function(cfg) {
-        const strategy = merge.strategy({
-            'plugins': 'prepend'
-        });
+  config: function (cfg) {
+    const strategy = merge.strategy({
+      'plugins': 'prepend'
+    });
 
-        return strategy (cfg, {
-            plugins: [
-                new webpack.DefinePlugin({
-                    "VERSION": JSON.stringify("4711")
-                })
-            ]
-        });
-    }
+    return strategy(cfg, {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inlineSource: '.(js|css)$'
+        }),
+        new HtmlWebpackInlineSourcePlugin()
+      ]
+    });
+  }
 }
