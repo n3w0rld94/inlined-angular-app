43d5c40181a9334bea519c9590662a62c52a841c
Ilyasse Fakhreddine <elias.faraone@outlook.com>
2020-04-07 04:05:20 +0200

    Added externals

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -1,20 +1,18 @@
-var merge = require('webpack-merge');
-var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
-exports.default = {
-  config: function (cfg) {
-    const strategy = merge.strategy({
-      'plugins': 'prepend'
-    });
-
-    return strategy(cfg, {
-      plugins: [
-        new HtmlWebpackPlugin({
-          inlineSource: '.(js|css)$'
-        }),
-        new HtmlWebpackInlineSourcePlugin()
-      ]
-    });
-  }
+module.exports = {
+  "externals": {
+    "rxjs": "rxjs",
+    "@angular/core": "ng.core",
+    "@angular/common": "ng.common",
+    "@angular/platform-browser": "ng.platformBrowser",
+    "@angular/elements": "ng.elements"
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inlineSource: '.(js|css)$'
+    }),
+    new HtmlWebpackInlineSourcePlugin()
+  ]
 }
