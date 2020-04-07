a49741874cfe46377be13b9d3dbc1d259c6e70d4
Ilyasse Fakhreddine <elias.faraone@outlook.com>
2020-04-07 04:17:06 +0200

    added extra webpack config hooks

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -2,13 +2,6 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 var HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
-  "externals": {
-    "rxjs": "rxjs",
-    "@angular/core": "ng.core",
-    "@angular/common": "ng.common",
-    "@angular/platform-browser": "ng.platformBrowser",
-    "@angular/elements": "ng.elements"
-  },
   plugins: [
     new HtmlWebpackPlugin({
       inlineSource: '.(js|css)$'
