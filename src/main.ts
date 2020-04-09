5f37b6fd7c22ec23036926c936942f6cc2ede875
Ilyasse Fakhreddine <elias.faraone@outlook.com>
2020-04-09 18:51:55 +0200

    solved multiple bootstrapping of angular app

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import 'zone.js/dist/zone';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+if (!window['angulr-app-started-already']) {
+  console.log("Bootstrapped")
+  window['angulr-app-started-already'] = true;
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+}
